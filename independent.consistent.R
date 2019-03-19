b1f259ea9a7635ded9731899d66fb4ec86d710b1
lian09 <48574150+lian09@users.noreply.github.com>
2019-03-19 16:39:41 +0800

    Add files via upload
    The code for independent testing when the data type of training set and test set is consistent

---
+++ independent.consistent.R
@@ -0,0 +1,30 @@
+library(randomForest)
+library(caret)
+library(pROC)
+independent_consistent<-function(train,test,dir,filename){
+  #train
+  print("Predicting...")
+  rf <- randomForest(Target_label ~ ., data=train)
+  classification=predict(rf,newdata=test)
+  pro=predict(rf,newdata=test,type="prob")
+  TP <- as.numeric(sum(classification=="m6A_positive" & test$Target_label=="m6A_positive"))
+  FP <- as.numeric(sum(classification=="m6A_positive" & test$Target_label=="m6A_negative"))
+  TN <- as.numeric(sum(classification=="m6A_negative" & test$Target_label=="m6A_negative"))
+  FN <- as.numeric(sum(classification=="m6A_negative" & test$Target_label=="m6A_positive"))
+  TPR=TP/(TP+FN)
+  FPR=TN/(TN+FP)
+  MCC=(TP*TN-FP*FN)/sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))
+  F1=(2*TP)/(2*TP+FP+FN)
+  ACC=(TP+TN)/(TP+TN+FP+FN)
+  precision=TP/(TP+FP)
+  recall=TP/(TP+FN)
+  ROCArea=auc(roc(test$Target_label,as.numeric(pro[,2])))
+  prob=pro[,2]
+  
+  result=data.frame(TPR,FPR,F1,precision,ACC,recall,MCC,ROCArea)
+  colnames(result)<-c("TPR","FPR","F-Measure","Precision","ACC","Recall","MCC","AUC")
+  write.csv(result,paste(dir,paste(filename,".result.csv"),sep = '/'),row.names = F)
+  write.csv(pro[,2],paste(dir,paste(filename,".probability.csv"),sep = '/'),row.names = F)
+  
+  return(result)
+}
